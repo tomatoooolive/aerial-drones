41d63277645b93f3b52d958b2d4c74777a61a619
Ayesha <mst.ayesha1702@gmail.com>
2025-06-02 18:57:41 +0300

    As whole project folder

---
+++ build/tello_driver/colcon_command_prefix_build.sh
@@ -0,0 +1,3 @@
+# generated from colcon_core/shell/template/command_prefix.sh.em
+. "/home/armrs/drone_racing_ros2_ws/install/ros2_shared/share/ros2_shared/package.sh"
+. "/home/armrs/drone_racing_ros2_ws/install/tello_msgs/share/tello_msgs/package.sh"
---
+++ build/tello_gazebo/colcon_command_prefix_build.sh
@@ -0,0 +1,2 @@
+# generated from colcon_core/shell/template/command_prefix.sh.em
+. "/home/armrs/drone_racing_ros2_ws/install/tello_msgs/share/tello_msgs/package.sh"
